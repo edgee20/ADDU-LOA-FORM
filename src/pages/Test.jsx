2e37cc9dfad037c41ce35a595aacc768b33c1481
Sam Tesoro <Sam@anthonys-MacBook-Pro.local>
2026-05-08 11:41:01 +0800

    feature: dropdown component

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,7 +1,7 @@
-
+import DropDown from "../components/DropDown";
 
 export default function Test(){
     return(<div className="">
-
+            <DropDown fieldName="Current Course" />
         </div>);
 }
