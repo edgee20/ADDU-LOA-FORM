150d4c2407611be9ce8aa631463f0f7c595a6343
kajcs <kajcudal@addu.edu.ph>
2026-05-08 08:14:14 +0800

    Created a Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/ui/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h1 className="text-9xl">Hello World!</h1>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/ui/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer
+      className="py-8 shadow-md"
+      style={{ backgroundColor: "#2F3590" }}
+    >
+    </footer>
+  );
+}
