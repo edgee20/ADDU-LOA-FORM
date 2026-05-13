820b78acb567960a14e55b5e0bc110058bdefee6
kajcs <kajcudal@addu.edu.ph>
2026-05-13 09:24:13 +0800

    fixed: merge conflict

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import adduLogo from "../assets/addu-white.png";
 import UserInfo from "./UserInfo";
 
 export default function Navbar({ user }) {
-
   return (
     <header
       className="flex justify-between items-center px-20 py-6 shadow-md"
@@ -13,7 +12,7 @@ export default function Navbar({ user }) {
         <img src={adduLogo} alt="ADDU Logo" className="h-22 w-22" />
         <div
           className="text-white"
-          style={{ fontFamily: "'Trajan Pro'", fontWeight: 300 }}
+          style={{ fontFamily: "'Trajan'", fontWeight: 300 }}
         >
           <div className="text-4xl">ATENEO DE DAVAO UNIVERSITY</div>
           <div className="text-[15px]">STRONG IN FAITH THAT DOES JUSTICE </div>
